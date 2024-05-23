2b42d86a72d26b4005211f648452c20273cf3060
kibarke <kibarkework@gmail.com>
2024-05-23 19:32:17 -0400

    twisted it to almost need

--- client/src/pages/order.jsx
+++ client/src/pages/order.jsx
@@ -4,16 +4,16 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import CommentList from '../components/CommentList';
-import CommentForm from '../components/CommentForm';
+import Order from '../components/order';
+import User from '../components/user'; // ? When we want user, do we want a history of their order??
 
-import { QUERY_SINGLE_THOUGHT } from '../utils/queries';
+import { ORDER_SINGLE_ITEM } from '../utils/queries';
 
 const SingleThought = () => {
   // Use `useParams()` to retrieve value of the route parameter `:profileId`
   const { thoughtId } = useParams();
 
-  const { loading, data } = useQuery(QUERY_SINGLE_THOUGHT, {
+  const { loading, data } = useQuery(ORDER_SINGLE_ITEM, {
     // Pass the `thoughtId` URL parameter into query to retrieve this thought's data
     variables: { thoughtId: thoughtId },
   });
@@ -46,10 +46,10 @@ const SingleThought = () => {
       </div>
 
       <div className="my-5">
-        <CommentList comments={thought.comments} />
+        <Order comments={thought.comments} />
       </div>
       <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
-        <CommentForm thoughtId={thought._id} />
+        <User thoughtId={thought._id} />
       </div>
     </div>
   );
