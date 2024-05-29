49c2cbe36a5fc3ed962c22af6a4b7742f2fe8cde
kibarke <kibarkework@gmail.com>
2024-05-29 19:16:22 -0400

    tutor help

---
+++ client/src/components/menu/index.jsx
@@ -0,0 +1,19 @@
+//  Stolen code from 21-MERN => 01-Activities => Solved => client => src => components => header
+
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <Link className="text-light" to="/">
+          <h1 className="m-0">Pick Your Poison</h1>
+        </Link>
+        <p className="m-0">The toxic relationship you need!</p> 
+        {/* IDK what to put there, just a brand motto */}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
