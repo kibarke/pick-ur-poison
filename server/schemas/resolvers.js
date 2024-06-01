bdddbbcc00cc1cc5705a749bf54a1b27a06d4f53
Paris Woods <148830068+pwoods125@users.noreply.github.com>
2024-06-01 13:27:17 -0400

    Fixed Null working on cart more

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,8 @@
 const { Query } = require('mongoose');
 const { User, Cocktail, Mocktail, Cart } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
+const stripe = require('stripe')('');
+
 
 const resolvers = {
     Query: {
@@ -22,7 +24,27 @@ const resolvers = {
         cocktail: async (parent, { _id }) => {
           return await Cocktail.findById(_id);
         },
-
+        checkout: async (parent, args, context) => {
+          const url = new URL(context.headers.referer).origin;
+          // eslint-disable-next-line camelcase
+          const line_items = {
+            price_data: {
+              currency: 'usd',
+              unit_amount: args.total,
+            },
+          };
+    
+          const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items,
+            mode: 'payment',
+            success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${url}/`,
+          });
+    
+          return { session: session.id };
+        },
+    
     },
 
     Mutation: {
