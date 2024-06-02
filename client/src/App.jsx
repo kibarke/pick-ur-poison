8221605f26ee7275640192039cb18da40a4553c0
kibarke <kibarkework@gmail.com>
2024-06-02 12:59:50 -0400

    routing correctly, able to add to cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar'
-import Dashboard from './pages/Dashboard'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
@@ -13,9 +12,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="flex-column justify-center align-center min-100-vh bg-primary">
-      {/* <Navbar></Navbar>
-      <Dashboard></Dashboard> */}
-      <Menu />
+      <Navbar></Navbar>
         <Outlet />
       </div>
     </ApolloProvider>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import Home from './pages/Home.jsx'
+import ErrorPage from './pages/Error.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
